c90170595a172a55a56b48830380a0d93a804b38
George Kargiotakis <kargig@void.gr>
2017-08-03 21:07:25 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# vrespiti
+
+Scrape xe.gr to find a house to rent the Linux shell way.
+
+## Why ?
+
+- Since xe.gr offers the ability to subscribe to searches and send you emails, why does this script exist ?
+- Most people who rent houses don't categorize their properties very well, especially regarding heating. It is
+very common to add the type of heating, "αυτόνομη θέρμανση" or "κεντρική θέρμανση" inside the description and not
+as a selectable filter. On the other hand xe.gr doesn't allow you to filter based on keywords inside the 
+descriptions of ads, so you end up opening useless URLs just to find out they have "κεντρική θέρμανση" that you
+don't want.
+
+And because writing a script in bash to parse html as json sounds cool.
+
+## Requirements
+
+ * A Linux box capable of sending emails.
+ * Install [pup](https://github.com/ericchiang/pup)
+ * Install jq and mailx (apt install jq bsd-mailx)
+
+## How to use
+
+ * Go to xe.gr and create your own filter for houses to rent. Choose whatever filtering criteria you want.
+ * Get the URL from your browser and place it inside vresspiti.sh
+ * Adjust RECIPIENTS of the emails to be sent. 
+ * Adjust WORK_PATH to a directory that the script can write files.
+ * Create a cron entry to run the shell script as often as you want.
+ * You might want to wrap the shell script with torsocks in case xe.gr tries to ban your IP for too many connections.
+
+## Did it work ?
+
+Yeap! I got a great new house using this script ;)
+
+## Similar work
+
+I've heard that people have written their own versions of this in php and python but I'm not aware of any public versions.
---
+++ vresspiti.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+RECIPIENTS="youremail@address.com,andonemore@newaddress.com"
+WORK_PATH="/home/yourusername/myhouses"
+URL="http://www.xe.gr/property/search?System.item_type=re_residence&Transaction.type_channel=117541&Transaction.price.from=300&Transaction.price.to=500&Item.area.from=40&Item.area.to=100&Geo.area_id_new__hierarchy=83268"
+
+mkdir -p "${WORK_PATH}/tmp"
+rm -f ${WORK_PATH}/tmp/array.out
+rm -f ${WORK_PATH}/tmp/tomail
+
+curl "${URL}" | pup '[class="r_desc"] json{}' > ${WORK_PATH}/tmp/newout
+jq -r '[.[] as $house | [$house.children[0].children[0].href,$house.children[1].text]]' < ${WORK_PATH}/tmp/newout| \
+	sed -e 's/"//g' | sed -e 's/\(\/pro\)/https:\/\/www.xe.gr\1/g'         | \
+	sed -e 's/],*//g' -e 's/\[//g' -e '/^\s*$/d' -e 's/,$//g' -e 's/^\s*//g' \
+	> ${WORK_PATH}/tmp/array.out
+
+IFS=$'\r\n' GLOBIGNORE='*' command eval  'HOUSES=($(cat ${WORK_PATH}/tmp/array.out))'
+ARRLEN=${#HOUSES[@]}
+
+i=0
+while [ ${i} -lt ${ARRLEN} ]; do
+	# get the links for the houses, string must contain http
+	echo "${HOUSES[$i]}" | grep -q "http"
+	if [ "$?" -eq "0" ]; then
+		# filter out houses that we have already sent email for
+		grep -q "${HOUSES[$i]}" ${WORK_PATH}/mailed
+		if [ "$?" -gt "0" ]; then
+			# only continue if the description of the house doesn't contain "κεντρικ. θ.ρμανση"
+			echo "${HOUSES[$((i+1))]}" | egrep -q "κεντρικ. θ.ρμανση"
+			if [ "$?" -gt "0" ]; then
+				echo "New house ${HOUSES[$i]}" >> ${WORK_PATH}/tmp/tomail
+				echo "${HOUSES[$((i+1))]}" >> ${WORK_PATH}/tmp/tomail
+			fi
+		fi
+	fi
+	i=$(( $i + 1 ))
+done
+
+
+if [ -f ${WORK_PATH}/tmp/tomail ]; then 
+	cat ${WORK_PATH}/tmp/tomail | mailx -s "new houses" -r noreply@xe.gr ${RECIPIENTS}
+	grep http ${WORK_PATH}/tmp/tomail >> ${WORK_PATH}/mailed
+	rm -f ${WORK_PATH}/tmp/tomail
+fi
